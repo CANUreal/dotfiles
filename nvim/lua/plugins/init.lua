0f876ee0d7127ac475d78426af7d4c475a142d90
CANUreal <dnzdyn2012@gmail.com>
2026-08-23 12:00:31 +0300

    added lualine.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -151,5 +151,17 @@ return {
             vim.cmd.colorscheme("nightfly")
         end,
     },
-
-}
+    -- lualine bottom bar
+    {
+    	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		 require("lualine").setup({
+		  options = {
+			  theme = "auto",
+			  section_separators = "",
+			  component_separators = "",
+			    },
+			})
+	end,
+    }
