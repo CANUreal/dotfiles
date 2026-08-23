395fc51bb13b2dfa9e8431dbafd50a04b5d58b10
CANUreal <dnzdyn2012@gmail.com>
2026-08-23 12:03:21 +0300

    fixed syntax error in nvim config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -151,17 +151,19 @@ return {
             vim.cmd.colorscheme("nightfly")
         end,
     },
-    -- lualine bottom bar
+    
     {
-    	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		 require("lualine").setup({
-		  options = {
-			  theme = "auto",
-			  section_separators = "",
-			  component_separators = "",
-			    },
-			})
-	end,
-    }
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "auto", 
+                section_separators = "",
+                component_separators = "",
+            },
+        })
+    end,
+},
+
+} 
